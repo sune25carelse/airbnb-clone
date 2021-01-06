763a380604c31c2d1066d96c2dac12516d5b6345
Sune Carelse <sune.carelse@takealot.com>
2021-01-06 23:59:55 +0200

    style home and header icons / center and right sid

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import  { Avatar } from "@material-ui/core";
 
 function Header() {
   return (
     <div className="header">
       <img
         className="header__icon" 
-        src="https://mybroadband.co.za/news/wp-content/uploads/2015/07/Airbnb-logo.jpg"
+        src="https://assets.entrepreneur.com/images/misc/1540931255_airbnb-logos.jpg"
         alt=""
       />
 
       <div className="header__center">
         <input type="text"/>
         <SearchIcon />
+      </div>
+
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
       </div>
     </div>
   );
